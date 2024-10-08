a83ade089c9a91acba2650854294508703da277e
Laura <88065630+Artixis@users.noreply.github.com>
2024-10-08 14:50:26 +1100

    Add files via upload

---
+++ merg.py
@@ -0,0 +1,13 @@
+'''
+v simple script to merge
+Do this at the end, once I've sorted out manual process for each school.
+'''
+
+import pandas as pd 
+
+df1 = pd.read_excel('Scholarships Research.xlsx')
+df2 = pd.read_excel('scraped_UNSW_data.xlsx')
+
+merged_df = pd.concat([df1, df2], ignore_index=True)
+
+merged_df.to_excel('merged_fule.xlsx', index=False)
