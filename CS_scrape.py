24bed0f2cc448a82e1df8ac5d3fdd46425e61b7c
Laura Pierce <30064037@westernsydney.edu.au>
2024-10-22 15:37:58 +1100

    Ran cs_scrape and adjusted code due to xsxl output

--- CS_scrape.py
+++ CS_scrape.py
@@ -13,22 +13,44 @@ equity_keys = ['uac', 'equity', 'ES']
 
 PG_keys = ['pg', 'master', 'postgraduate', 'masters']
 UG_keys = ['ug', 'bachelor', 'undergraduate']
-other_keys = ['phd', 'diploma', 'doctorate', 'honours']
-
-indigenous_keys = ['indigenous', 'ATSI', 'aboriginal']
+other_keys = ['please note','enrolled full-time','final year','phd', 'diploma', 'doctorate', 'honours']
+criteria_keys = ['the intention of','1st year','commonwealth supported student','any undergraduate','continued enrolment','notes:','will be paid as','must be studying','note:','campus','any postgraduate', 'conditions', 'preference to', 'course offered', '2nd / 3rd', '1st/ 2nd', 'study mode']
+indigenous_keys = ['indigenous', 'ATSI', 'aboriginal', 'first nations']
 data_raw = []
+
+def format_criteria(criteria_list):
+    return '\n'.join([f'• {criteria}' for criteria in criteria_list])
+
+def create_hyperlink(page_url, name):
+    hyperlink = f'=HYPERLINK("{page_url}", "{name}")'
+    return(hyperlink)
+
+def create_data_entry(url, type_of, value, level, criteria, duration="NA", indigenous="No", placement="No"):
+    new_entry = {'University': "UOW",
+                 'Name': url,
+                 'Type': type_of,
+                 'Value': value,
+                 'Duration': duration,
+                 'Level': level,
+                 'Criteria': criteria,
+                 'Indigenous': indigenous,
+                 'Placement': placement}
+    data_raw.append(new_entry)
 
 def check_duration(value_info):
     duration = "na"
     if "duration" in value_info.lower():
         duration = "Duration of degree"
-    elif "one off" in value_info.lower():
+    elif "one off" in value_info.lower() or "one-off" in value_info.lower():
         duration = "One-off"
     elif "instalments" in value_info.lower():
         duration ="1 years"
     elif "year" in value_info.lower() or "years" in value_info.lower():
         year_num = re.search(r'(\d+)', value_info.lower())
-        duration = f"{year_num.group(1)} years"
+        if year_num:
+            duration = f"{year_num.group(1)} years"
+        else:
+            duration = "1 years"
     else:
         duration = "na"
     return duration
@@ -53,10 +75,6 @@ def check_level(given_text):
     else:
         return "Undefined"
 
-# TODO: Move later. This will hold phrases that need to be excluded from criteria
-#  
-criteria_keys = ['note:','campus','any postgraduate', 'conditions', 'preference to', 'course offered', '2nd / 3rd', '1st/ 2nd', 'study mode']
-
 def clean_criteria(given_points):
     filtered_critera = []
     for point in given_points:
@@ -69,10 +87,10 @@ def clean_criteria(given_points):
             filtered_critera[i] = "Rural or Regional"
         elif "female" in filtered_critera[i].lower():
             filtered_critera[i] = "Female"
-        if "academic performance" in filtered_critera[i].lower() or "sound academic record" in filtered_critera[i].lower() or "atar" in filtered_critera[i].lower():
+        if "academic performance" in filtered_critera[i].lower() or "sound academic record" in filtered_critera[i].lower() or "atar" in filtered_critera[i].lower() or "gpa" in filtered_critera[i].lower():
             filtered_critera[i] = "Academic merit"
         
-        if "financial hardship" in filtered_critera[i].lower():
+        if "financial hardship" in filtered_critera[i].lower() or "financial need" in filtered_critera[i].lower():
             filtered_critera[i] = "Financial hardship"
         if "educational challenges" in filtered_critera[i].lower() or "equity scholarships" in filtered_critera[i].lower() or "equity application" in filtered_critera[i].lower():
             filtered_critera[i] = "Equity criteria"
@@ -80,9 +98,8 @@ def clean_criteria(given_points):
             filtered_critera[i] = "Provide statement"
         if "disability" in filtered_critera[i].lower():
             filtered_critera[i] = "Disability" 
-        if "answer the following" in filtered_critera[i].lower():
+        if "outline your" in filtered_critera[i].lower()or "career goals" in filtered_critera[i].lower() or "answer the following" in filtered_critera[i].lower():
             filtered_critera[i] = "Provide a statement" 
-        
     
     formatted_data = list(set(filtered_critera))
     return formatted_data
@@ -118,17 +135,19 @@ def CS_check_crit(given_soup):
     return cleaned_list
 
 
-def search_page(page_url):
+def search_page(page_url, page_name):
     response = requests.get(page_url)
     soup = BeautifulSoup(response.content, "html.parser")
-    scholarship_name = soup.find('div', class_='container first').get_text(strip=True)
-    
+    scholarship_name = page_name
+
     has_indig = any(key in scholarship_name.lower() for key in indigenous_keys)
     if has_indig:
         return
 
     if "placement" in scholarship_name.lower() or "work integrated" in scholarship_name.lower():
         placem="Yes"
+    else:
+        placem="No"
 
     schol_details = soup.find('div', class_='col-wrap-1a w-col-4')
     strong_elements = schol_details.find_all('strong')
@@ -140,7 +159,8 @@ def search_page(page_url):
         if "Duration" in key:
             dur_text = strong.next_sibling.strip()
             duration = check_duration(dur_text)
-    
+        else:
+            duration = "NA"
     # The pages have hidden html with unique (maybe) codes for each tab
     # Hence the need to use select and match with tab1_
     eligibility_div = soup.select('div[id^="tab1_"]')
@@ -154,20 +174,39 @@ def search_page(page_url):
     eligi_done = clean_criteria(criteria_list)
     # remove empties
     eligi_done = [elem for elem in eligi_done if elem]
-    print(eligi_done)
+    #print(eligi_done)
     
-   # print("Val:", value, "\nDuration:", duration, "\nLevel:", level)
-# this one doesnt work
+    create_data_entry(create_hyperlink(page_url, page_name), scholarship_name, value, level, eligi_done, duration, "No", placem)
+
+
 test_url = "https://www.csu.edu.au/scholarships/scholarships-grants/find-scholarship/equity/charles-sturt-kickstart-scholarship"
 test_url2 = "https://www.csu.edu.au/scholarships/scholarships-grants/find-scholarship/foundation/any-year/vivability-limited-empowered-futures-scholarship"
 test_pg = "https://www.csu.edu.au/scholarships/scholarships-grants/find-scholarship/foundation/continuing/online-study-student-representative-committee-post-graduate-scholarship"
-search_page(test_pg)
+#search_page(test_pg)
 
 
-
-# TODO: Main iteration
-
+# TODO: Modify code to extract the name of the scholarship from here rather than the page/
+num_count = 0
 main_url = "https://www.csu.edu.au/scholarships?queries_status_query_posted=1&queries_status_query%5B0%5D=Commencing&queries_status_query%5B1%5D=Continuing&queries_type_query_posted=1&queries_type_query%5B0%5D=Internal&queries_levelofstudy_query_posted=1&queries_levelofstudy_query%5B0%5D=UG&queries_levelofstudy_query%5B1%5D=Hon&queries_levelofstudy_query%5B2%5D=PG&queries_faculty_query_posted=1&queries_faculty_query%5B0%5D=Arts+and+Education&queries_faculty_query%5B1%5D=Business%2C+Justice+%26+Behavioural+Sciences&queries_faculty_query%5B2%5D=Science&queries_studymode_query_posted=1&queries_studymode_query%5B0%5D=DE&queries_studymode_query%5B1%5D=OC&queries_studymode_query%5B2%5D=Mixed&queries_campus_query_posted=1&search_page_1218754_submit_button=Search"
-soup_main = BeautifulSoup(main_url, 'html.parser')
-#print(soup_main.find_all('a', href=True))
+response = requests.get(main_url)
+soup_main = BeautifulSoup(response.content, 'html.parser')
+table_info = soup_main.find('table', id='dataTable')
+rows = table_info.find_all('tr')
+for row in rows:
+        num_count +=1
+        tds = row.find_all('td')
+        if tds:  
+            first_td = tds[0]   
+            link = first_td.find('a')
+            if link and 'href' in link.attrs:
+                print(num_count)
+                search_page(link['href'], link.get_text())
 
+df = pd.DataFrame(data_raw)
+df['Criteria'] = df['Criteria'].apply(format_criteria)   
+df.to_excel("cs_test_scrape.xlsx", index=False)
+
+print("Done")
+
+
+# TODO: add code to iterate over the eligibility criteria to flag indigenous preference
