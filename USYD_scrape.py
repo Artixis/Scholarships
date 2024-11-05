c7c23d3635d84bbe5bc0528afa6fbaa3595f479b
Laura <19674777@student.westernsydney.edu.au>
2024-11-05 11:32:38 +1100

    Started work on USYD scrape - duration errors

--- USYD_scrape.py
+++ USYD_scrape.py
@@ -1,6 +1,6 @@
 import requests
 import re
-import pandas as pd
+import pickle
 from bs4 import BeautifulSoup
 
 value_pat = r'\$\d{1,3}(?:,\d{3})*'
@@ -18,9 +18,6 @@ def create_hyperlink(page_url, name):
 
 def format_criteria(criteria_list):
     return '\n'.join([f'• {criteria}' for criteria in criteria_list])
-
-def concatenate_url(base_url, href_url):
-    return f"{base_url}{href_url}"
 
 def check_level(given_text):
     if isinstance(given_text, list):
@@ -86,9 +83,22 @@ word_to_num = {
     "ten": 10
 }
 
+# TODO: fix the ordering of this code. messy
+
 def get_duration(tnc_soup):
     value_section = tnc_soup.find_all(string="4. Value")[0].find_next('p')
+    if not value_section.get_text(strip=True):
+        value_section = value_section.find_next('p', string=lambda text: text and text.strip())
+    print(value_section)
+    
     duration_text = value_section.get_text()
+    
+    if "determined by the Dean" in duration_text or "determined by the Head of School" in duration_text:
+        return ("Determined by Dean")
+    
+    if "duration of the" in duration_text:
+        return ("Duration of Degree")
+    
     match = re.search(r'(\d+|\b(?:one|two|three|four|five|six|seven|eight|nine|ten)\b)\s*year(s)?', duration_text)
     if match:
         duration_years = match.group(1)
@@ -101,15 +111,16 @@ def get_duration(tnc_soup):
     
     return(duration)
 
-def search_page(base_url, page_url):
-
-    url = concatenate_url(base_url, page_url)
+def search_page(page_url):
     equity = False
     indigen = "No"
     type_of_schol = "NULL"
 
     response = requests.get(page_url)
     soup = BeautifulSoup(response.content, "html.parser")
+    title_tag = soup.find('title')
+    if title_tag and "404 resource" in title_tag.string.lower():
+        return
 
     scholarship_name = soup.find('h1', class_='pageTitle').get_text(strip=True)
     if "travel" in scholarship_name.lower():
@@ -132,23 +143,17 @@ def search_page(base_url, page_url):
     #print(summary_text)
     create_data_entry("USYD", create_hyperlink(page_url, scholarship_name), type_of_schol, val, check_level(summary_text), eligibility, duration = duration, indigenous = indigen)
 
-test_url = "https://www.sydney.edu.au/scholarships/e/david-clarke-memorial-scholarship.html"
 
+with open('my_urls.pkl', 'rb') as file:
+    url_list = pickle.load(file)
+url_list = sorted(url_list)
 
-base_url = "https://www.sydney.edu.au/scholarships/"
-# This covers Bachelors and Honours scholarships
-general_url = "https://www.sydney.edu.au/scholarships/domestic/bachelors-honours/general.html"
-response = requests.get(general_url)
-soup_general = BeautifulSoup(response.content, 'html.parser')
-# Doing this ensures we  only get the urls we need. 
-main_section = soup_general.find('div', class_='bodyColumn')
-urls = [a['href'] for a in main_section.find_all('a', href=True) if a['href'].startswith('/content/corporate/')]
-modified_urls = [url.replace('/content/corporate/scholarships/', '') for url in urls]
+test_num = 0
 
-for url in modified_urls:
-    print(concatenate_url(base_url,url))
+for url in url_list:
+    if test_num < 360:
+        print("**\Test Num: ", test_num, "\n**")
+        print(url)
+        search_page(url)
+    test_num += 1
 
-
-'''
-Currently, the duration function does not consider the DOD or the One-off
-'''
